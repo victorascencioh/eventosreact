55783c29227cdb0ccf2758ad5974a8b1a82b2262
victorascencioh <victorascencioh@gmail.com>
2018-12-06 21:51:53 +0100

    Updates

---
+++ precache-manifest.4d0c8f134ad43c55956ae6ddbcf4993e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "13e07d44e03184186d59",
+    "url": "/eventosreact/static/js/runtime~main.13e07d44.js"
+  },
+  {
+    "revision": "c6c18c8375529c0a04f2",
+    "url": "/eventosreact/static/js/main.c6c18c83.chunk.js"
+  },
+  {
+    "revision": "a5af75791534beb14c1a",
+    "url": "/eventosreact/static/js/1.a5af7579.chunk.js"
+  },
+  {
+    "revision": "c6c18c8375529c0a04f2",
+    "url": "/eventosreact/static/css/main.4544f7fa.chunk.css"
+  },
+  {
+    "revision": "ccd15cd3fe7c27d75566f6fbf192733d",
+    "url": "/eventosreact/index.html"
+  }
+];
